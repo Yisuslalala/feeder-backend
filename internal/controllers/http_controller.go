199197072515bc8ca827543746aba6bcdf32e80f
Yisuslalala <wardodev1.00@gmail.com>
2025-03-29 23:29:06 -0600

    controllers and routers added

---
+++ internal/controllers/http_controller.go
@@ -0,0 +1,66 @@
+package controllers
+
+import (
+	"fmt"
+
+	"database/sql"
+	"encoding/json"
+	"feeder-backend/internal/config"
+	"feeder-backend/internal/models"
+	"net/http"
+)
+
+func GetFeedDetails(w http.ResponseWriter, r *http.Request) {
+		
+	details, err := getDetails()
+	if err == nil {
+		responseSuccess(details, w)
+	} else {
+		responseError(err, w)
+	}
+}
+
+func getDetails() ([]models.Detail, error) {
+	
+  details := []models.Detail{}
+	
+	endpointString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+	config.HTTPQueries["user"],
+	config.HTTPQueries["pass"],
+	config.HTTPQueries["host"],
+	config.HTTPQueries["port"],
+	config.HTTPQueries["dbName"],
+	)
+
+	db, err := sql.Open("mysql", endpointString)
+	if err != nil {
+		return details, err
+	}
+
+	rows, err := db.Query("SELECT id, feed_at FROM feeder_details")
+	if err != nil {
+		return details, err
+	}
+
+	for rows.Next() {
+		var detail models.Detail
+		err = rows.Scan(&detail.ID, &detail.FeedAt)
+		if err != nil {
+			return details, err
+		}
+
+		details = append(details, detail)
+	}
+
+	return details, nil
+}
+
+func responseSuccess(data interface{}, w http.ResponseWriter) {
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(data)
+}
+
+func responseError(err error, w http.ResponseWriter) {
+	w.WriteHeader(http.StatusInternalServerError)
+	json.NewEncoder(w).Encode(err.Error())
+}
---
+++ internal/routers/feeder_routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"feeder-backend/internal/controllers"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func RegisterFeederRoutes(r *mux.Router) {
+	r.HandleFunc("/details", controllers.GetFeedDetails).Methods(http.MethodGet)
+}

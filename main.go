90e388a863ea532e248a529c001b1502d4530ec4
Yisuslalala <wardodev1.00@gmail.com>
2025-03-29 23:26:29 -0600

    main modified and working

--- main.go
+++ main.go
@@ -1,61 +1,26 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
-
 	"feeder-backend/internal/db"
-
+	"feeder-backend/internal/mqtt"
+	routes "feeder-backend/internal/routers"
 	"fmt"
-	"log"
 	"net/http"
 
 	_ "github.com/eclipse/paho.mqtt.golang"
-	mqtt "github.com/eclipse/paho.mqtt.golang"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
-var mqttClient mqtt.Client
 
 func main() {
 	db.InitDB()
 	
-
-
-  broker := "tcp://192.168.1.125:1883" // Change if using a different host
-	clientID := "go_mqtt_server"
-
-	// Set up MQTT options
-	opts := mqtt.NewClientOptions().AddBroker(broker).SetClientID(clientID)
-
-	// Create and connect the MQTT client
-	mqttClient = mqtt.NewClient(opts)
-	token := mqttClient.Connect()
-	token.Wait()
-	if token.Error() != nil {
-		log.Fatal("MQTT Connection Error:", token.Error())
-	}
-	fmt.Println("Connected to MQTT broker")
+	mqtt.InitMQTT()
 
 	r := mux.NewRouter()
 	
-	r.HandleFunc("/details", func(w http.ResponseWriter, r *http.Request) {
-		
-		details, err := getDetails()
-		if err == nil {
-			responseSuccess(details, w)
-		} else {
-			responseError(err, w)
-		}
-
-	}).Methods(http.MethodGet)
-	
-	r.HandleFunc("/feed", func(w http.ResponseWriter, r *http.Request) {
-		publishMessage("/motor", "ON")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Motora activated"))
-	})
+	routes.RegisterFeederRoutes(r)
 
 	port := ":8000"
 
@@ -63,50 +28,7 @@ func main() {
 	Handler: r,
 	Addr: port,
 	}
-
+	
 	fmt.Println("Server started at " + port)
 	fmt.Println(s.ListenAndServe())
-}	
-
-func getDetails() ([]Detail, error) {
-	
-  details := []Detail{}
-		
-	db, err := sql.Open("mysql", "yisus:1235@tcp(192.168.1.125:3306)/feeder")
-	if err != nil {
-		return details, err
-	}
-
-	rows, err := db.Query("SELECT id, feed_at FROM feeder_details")
-	if err != nil {
-		return details, err
-	}
-
-	for rows.Next() {
-		var detail Detail
-		err = rows.Scan(&detail.id, &detail.feed_at)
-		if err != nil {
-			return details, err
-		}
-
-		details = append(details, detail)
-	}
-
-	return details, nil
-}
-
-func responseSuccess(data interface{}, w http.ResponseWriter) {
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(data)
-}
-
-func responseError(err error, w http.ResponseWriter) {
-	w.WriteHeader(http.StatusInternalServerError)
-	json.NewEncoder(w).Encode(err.Error())
-}
-
-func publishMessage(topic, payload string) {
-  token := mqttClient.Publish(topic, 0, false, payload)
-  token.Wait()
-  fmt.Println("Message published", payload)
 }

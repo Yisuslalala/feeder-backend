81731d9148cbface3f1e2d0c2a2dc38e5759eb93
Yisuslalala <wardodev1.00@gmail.com>
2025-03-31 20:37:43 -0600

    Unnecessary code removed

--- internal/db/db_connection.go
+++ internal/db/db_connection.go
@@ -12,14 +12,9 @@ import (
 var DB *sql.DB
 
 func InitDB() {
-	// err := godotenv.Load(".env") // Load environment variables
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
-  fmt.Println("DBConfig content:", config.DBConfig)
+  // fmt.Println("DBConfig content:", config.DBConfig)
   config.LoadEnv()
 
-	// Construct connection string
 	ConnectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
 		config.DBConfig["user"],
 		config.DBConfig["pass"],
@@ -28,22 +23,12 @@ func InitDB() {
 		config.DBConfig["dbName"],
 	)
 
-  for key, value := range config.DBConfig {
-    fmt.Println("Firts key", key)
-    fmt.Println("First value", value)
-  }
-
-  fmt.Println(`string`)
-  fmt.Println(config.DBConfig)
-  fmt.Println(`This is the connection string: `, ConnectionString)
-
   var err error
 	DB, err = sql.Open("mysql", ConnectionString)
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
 
-	// Test the connection
 	if err := DB.Ping(); err != nil {
 		log.Fatal("Database connection is not active:", err)
 	}

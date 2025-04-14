5f23b6124d2675e2fca963ce1af8ae79da3938b9
Yisuslalala <wardodev1.00@gmail.com>
2025-04-13 21:38:18 -0600

    First version of server file with approach of handlers

---
+++ internal/server/server.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+  "feeder-backend/internal/config"
+  cors "feeder-backend/internal/middleware"
+
+  "fmt"
+  "net/http"
+
+  "github.com/gorilla/mux"
+)
+
+func StartServer(r *mux.Router) error {
+  
+  var port = ":" + config.ServerConfig["port"]
+
+	// s.Handler = r
+	// s.Addr = port
+
+  // s.ListenAndServe()
+	fmt.Println("Server started at " + port)
+	// fmt.Println(s.ListenAndServe())
+
+  corsHandler := cors.SetupCORS(r)
+
+  s := HandlerServer(port, corsHandler)
+
+  err := s.ListenAndServe()
+  if err != nil {
+    fmt.Println("Server failed start", err)
+  }
+
+  return nil
+}
+
+func HandlerServer(port string, handler http.Handler) *http.Server {
+
+  return &http.Server {
+    Addr: port,
+    Handler: handler,
+  }
+}

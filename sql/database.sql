c345417e9f397752f42af04373d9d34a9fb8d70e
Yisuslalala <wardodev1.00@gmail.com>
2025-12-14 22:32:34 -0600

    tables definition in database schema script was fixed

--- sql/database.sql
+++ sql/database.sql
@@ -10,29 +10,46 @@ CREATE TABLE IF NOT EXISTS  feeder_details(
 CREATE TABLE IF NOT EXISTS users (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100),
-  email VARCHAR(255) UNIQUE NOT NULL,
+  email VARCHAR(255) NOT NULL,
   password_hash VARCHAR(255) NOT NULL,
   role VARCHAR(50) DEFAULT 'user',
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  deleted_at TIMESTAMP NULL
+  deleted_at TIMESTAMP NULL,
 
   CONSTRAINT uq_users_email UNIQUE(email)
 );
 
-CREATE IF NOT EXISTS houses (
+CREATE TABLE IF NOT EXISTS houses (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100),
-  owner_user_id BIGINT UNSIGNED NOT NULL,
+  owner_user_id BIGINT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  deleted_at TIMESTAMP NULL
+  deleted_at TIMESTAMP NULL,
 
   CONSTRAINT fk_houses_owner
-    FOREIGN KEY (owner_user_id) REFFERENCES users(id)
+    FOREIGN KEY (owner_user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS feeders (
+  id BIGINT AUTO_INCREMENT,
+  house_id BIGINT NOT NULL,
+  mac_address CHAR(17) NOT NULL,
+  name VARCHAR(100),
+  pet_type VARCHAR(50),
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP NULL,
+
+  CONSTRAINT pk_feeders PRIMARY KEY (id),
+  CONSTRAINT fk_feeders_house
+    FOREIGN KEY (house_id)
+    REFERENCES houses(id),
+  CONSTRAINT uq_feeders_mac UNIQUE (mac_address)
+
 );
 
 CREATE TABLE IF NOT EXISTS house_users (
-  house_id BIGINT UNSIGNED NOT NULL,
-  user_id BIGINT UNSIGNED NOT NULL,
+  house_id BIGINT NOT NULL,
+  user_id BIGINT NOT NULL,
   role ENUM('owner', 'admin', 'member') NOT NULL DEFAULT 'member',
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -43,7 +60,7 @@ CREATE TABLE IF NOT EXISTS house_users (
     FOREIGN KEY (house_id)
     REFERENCES houses(id)
     ON DELETE CASCADE
-    ON UPDATE CASCADE
+    ON UPDATE CASCADE,
 
   CONSTRAINT fk_house_users_user
     FOREIGN KEY (user_id)
@@ -51,20 +68,4 @@ CREATE TABLE IF NOT EXISTS house_users (
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
-
-CREATE TABLE IF NOT EXISTS feeders (
-  id BIGINT UNSIGNED AUTO_INCREMENT,
-  house_id BIGINT UNSIGNED NOT NULL,
-  mac_address CHAR(17) NOT NULL,
-  name VARCHAR(100),
-  pet_type VARCHAR(50),
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  deleted_at TIMESTAMP NULL
-
-  CONSTRAINT pk_feeders PRIMARY KEY (id),
-  CONSTRAINT fk_house fk_feeders_house
-    FOREIGN KEY (house_idd) REFERENCES hosues(id),
-  CONSTRAINT uq_feeders_mac UNIQUE (mac_address)
-
-) ENGINE=InnoDB;
 

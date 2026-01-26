d7b7a1c77cc8694c6739e41a5cc135fa420a0fbd
Yisuslalala <wardodev1.00@gmail.com>
2026-01-26 11:36:23 -0600

    generate token utility added

--- internal/utils/jwt.go
+++ internal/utils/jwt.go
@@ -1,1 +1,28 @@
 package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"feeder-backend/internal/config"
+)
+
+type Claims struct {
+	Email  string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(email string) (string, error) {
+	claims := Claims{
+		Email:  email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	secret := config.JWT.Secret
+	return token.SignedString([]byte(secret))
+}
+

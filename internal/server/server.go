13c17199ab050c03bece3c847141316d991a84f6
Yisuslalala <wardodev1.00@gmail.com>
2026-02-04 19:16:06 -0600

    server improved

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,40 +1,40 @@
 package server
 
 import (
-  "feeder-backend/internal/config"
-  cors "feeder-backend/internal/middleware"
+	"feeder-backend/internal/config"
+	cors "feeder-backend/internal/middleware"
 
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 
-  "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 func StartServer(r *mux.Router) error {
 
-  var port = ":" + config.Server.Port
-	
+	var port = ":" + config.Server.Port
+
 	if port == "" {
 		port = "8000"
 	}
 
 	fmt.Println("Server started at " + port)
 
-  corsHandler := cors.SetupCORS(r)
-  s := HandlerServer(port, corsHandler)
+	corsHandler := cors.SetupCORS(r)
+	s := HandlerServer(port, corsHandler)
 
-  err := s.ListenAndServe()
-  if err != nil {
-    fmt.Println("Server failed start", err)
-  }
+	err := s.ListenAndServe()
+	if err != nil {
+		fmt.Println("Server failed start", err)
+	}
 
-  return nil
+	return nil
 }
 
 func HandlerServer(port string, handler http.Handler) *http.Server {
 
-  return &http.Server {
-    Addr: port,
-    Handler: handler,
-  }
+	return &http.Server{
+		Addr:    port,
+		Handler: handler,
+	}
 }

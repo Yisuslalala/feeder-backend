18aa593e049a74e0e8aadd90a71a6eba705c0dfc
Yisuslalala <wardodev1.00@gmail.com>
2026-02-04 19:15:44 -0600

    feeder route improved

--- internal/routers/feeder_routes.go
+++ internal/routers/feeder_routes.go
@@ -2,15 +2,13 @@ package routes
 
 import (
 	"feeder-backend/internal/controllers"
-  "net/http"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 func RegisterFeederRoutes(r *mux.Router, controller *controllers.FeederController) {
 	// r.HandleFunc("/details", controllers.GetFeedDetails).Methods(http.MethodGet)
-  /// r.HandleFunc("/details", controllers.CreateDetail).Methods(http.MethodPost)
+	/// r.HandleFunc("/details", controllers.CreateDetail).Methods(http.MethodPost)
 	r.HandleFunc("/feeders", controller.Create).Methods(http.MethodPost)
 }
-
-

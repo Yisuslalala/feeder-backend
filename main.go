a67b128375ea56851631a573632950172cb9ffc2
Yisuslalala <wardodev1.00@gmail.com>
2026-02-04 19:16:33 -0600

    user delegation in main added

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-
 	"log"
 
-	"feeder-backend/internal/db"
-	"feeder-backend/internal/mqtt"
-	routes "feeder-backend/internal/routers"
-  "feeder-backend/internal/server"
 	config "feeder-backend/internal/config"
 	"feeder-backend/internal/controllers"
+	"feeder-backend/internal/db"
+	"feeder-backend/internal/mqtt"
 	"feeder-backend/internal/repositories"
+	routes "feeder-backend/internal/routers"
+	"feeder-backend/internal/server"
 	"feeder-backend/internal/services"
 
 	_ "github.com/eclipse/paho.mqtt.golang"
@@ -18,11 +17,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
-
 func main() {
 
 	config.Load()
-	
+
 	database, err := db.NewMySQLConnection()
 	if err != nil {
 		log.Fatalf("error with database: %v", err)
@@ -31,15 +29,22 @@ func main() {
 	mqtt.InitMQTT()
 
 	feederRepo := repositories.NewFeederRepository(database)
-	
+
 	feederService := services.NewFeederService(feederRepo)
-	
+
 	feederController := controllers.NewFeederController(feederService)
+
+	userRepo := repositories.NewUserRepository(database)
+
+	userService := services.NewUserService(userRepo)
+
+	userController := controllers.NewUserController(userService)
 
 	r := mux.NewRouter()
 	routes.RegisterFeederRoutes(r, feederController)
-  
-  // routes.StartServer(r)
+	routes.RegisterUserRoutes(r, userController)
 
-  server.StartServer(r)
+	// routes.StartServer(r)
+
+	server.StartServer(r)
 }
